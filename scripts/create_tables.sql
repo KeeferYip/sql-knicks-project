31a3353df08a27720f72a148b8e1c2ddd69fe170
Keefer Yip <keeferbyip@gmail.com>
2026-07-19 18:22:34 -0700

    Added players and games tables

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,3 +1,13 @@
+-- TEMPORARY drop table 
+DROP TABLE IF EXISTS player_stats CASCADE;
+DROP TABLE IF EXISTS team_stats CASCADE;
+DROP TABLE IF EXISTS player_teams CASCADE;
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS games CASCADE;
+DROP TABLE IF EXISTS teams CASCADE;
+DROP TABLE IF EXISTS raw_teams CASCADE;
+
+
 -- Created staging tables for data import
 CREATE TABLE raw_teams (
 	teamid int,
@@ -10,18 +20,27 @@ CREATE TABLE raw_teams (
 );
 
 
-
 -- Created normalized tables for analyses with respective keys
-CREATE TABLE teams(
+CREATE TABLE teams (
 	team_id int PRIMARY KEY,
+	raw_team_id int UNIQUE NOT NULL,
 	team_name varchar UNIQUE NOT NULL
+	
 );
 
 CREATE TABLE players (
 	player_id int PRIMARY KEY,
-	player_name varchar NOT NULL,
-	team_id int NOT NULL,
+	player_name varchar NOT NULL
 	
+);
+
+-- need to make a player_teams table because players can be traded mid-season
+CREATE TABLE player_teams (
+	player_id int,
+	team_id int
+	
+	PRIMARY KEY (player_id, team_id),
+	FOREIGN KEY (player_id) REFERENCES players(player_id),
 	FOREIGN KEY (team_id) REFERENCES teams(team_id)
 	
 );
--- scripts/table_manipulation.sql
+++ scripts/table_manipulation.sql
@@ -1,10 +1,74 @@
 -- Filtered CSV for current relative NBA teams
 
-INSERT INTO teams (team_id, team_name)
+INSERT INTO teams (team_id, raw_team_id, team_name)
 SELECT 
-	ROW_NUMBER() OVER() AS team_id,
+	ROW_NUMBER() OVER(ORDER BY teamcity ASC) AS team_id,
+	teamid AS raw_team_id,
 	CONCAT(teamcity, ' ', teamname) AS team_name 
 FROM raw_teams
 WHERE league = 'NBA' AND seasonactivetill = 2100 AND teamid > 10000;
 
 
+-- Updated column data type to prevent data import error
+ALTER TABLE raw_player_info
+ALTER COLUMN "seriesGameNumber" TYPE varchar;
+
+-- Picking out unique player and team info from raw 
+
+INSERT INTO players (player_id, player_name)
+SELECT DISTINCT
+    r."personId",
+    CONCAT(r."firstName", ' ', r."lastName")
+FROM raw_player_info r
+WHERE r."gameDateTimeEst" > '2025-10-01'
+  AND (r."gameType" NOT IN ('Preseason', 'All-Star Game') OR r."gameType" IS NULL);
+
+
+-- populating player_teams with respective ids
+INSERT INTO player_teams (player_id, team_id)
+SELECT DISTINCT
+	r."personId",
+	t.team_id
+FROM raw_player_info AS r 
+JOIN teams AS t
+	ON CONCAT(r."playerteamCity", ' ', r."playerteamName") = t.team_name 
+WHERE r."gameDateTimeEst" > '2025-10-01'
+  AND (r."gameType" NOT IN ('Preseason', 'All-Star Game') OR r."gameType" IS NULL);
+	
+	
+-- Filtering raw_games for NBA games only and re-assigning IDs
+INSERT INTO games(
+game_id,
+home_team_id,
+away_team_id,
+home_score,
+away_score,
+winner_id
+)
+SELECT 
+	DISTINCT 
+	rg."gameId",
+	home.team_id AS home_team_id,
+	away.team_id AS away_team_id,
+	rg."homeScore" AS home_score,
+	rg."awayScore" AS away_score,
+	winner.team_id AS winner_id
+FROM rawgames AS rg
+
+-- Mapping home team to new team_id
+JOIN teams AS home 
+	ON rg."hometeamId" = home.raw_team_id
+
+-- Mapping away team to new team_id
+JOIN teams AS away
+	ON rg."awayteamId" = away.raw_team_id
+
+-- Joining winning team IDs
+JOIN teams AS winner
+	ON rg."winner" = winner.raw_team_id
+
+WHERE 
+	"gameDateTimeEst" > '2025-10-01'
+	AND ("gameType" NOT IN ('Preseason', 'All-Star Game') OR "gameType" IS NULL);
+	
+	

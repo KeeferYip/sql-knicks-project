f3272f2fd95428494e5c06fc8bbd847794ef4f6a
Keefer Yip <keeferbyip@gmail.com>
2026-07-28 13:30:14 -0700

    Update views.sql

--- scripts/views.sql
+++ scripts/views.sql
@@ -4,27 +4,36 @@ DROP VIEW team_game_total
 -- Create a view for player game total stats
 CREATE VIEW player_game_total AS
 SELECT 
-		p.player_name,
-		p.player_id,
-		ps.team_id,
-		t.team_name,
-		g.game_id,
-		g.game_type,
-		ps.minutes,
-		ps.points,
-		ps.rebounds,
-		ps.assists,
-		ps.steals,
-		ps.blocks,
-		ps.turnovers,
-		ps.plus_minus,
-		ps.fgs_made,
-		ps.fgs_attempted,
-		ps.three_pts_made,
-		ps.three_pts_attempted,
-		ps.fts_made,
-		ps.fts_attempted,
-		ps.usage_pct
+	p.player_name,
+	p.player_id,
+	ps.team_id,
+	t.team_name,
+	g.game_id,
+	g.game_type,
+-- Allows us to filter directly for specific player and opposing teams
+	CASE 
+		WHEN ps.team_id = g.home_team_id THEN g.away_team_id
+	ELSE g.home_team_id
+END AS opponent_team_id,
+	CASE 
+		WHEN ps.team_id = g.home_team_id THEN away_team_name
+	ELSE home.team_name
+END AS opponent_team_name,
+	ps.minutes,
+	ps.points,
+	ps.rebounds,
+	ps.assists,
+	ps.steals,
+	ps.blocks,
+	ps.turnovers,
+	ps.plus_minus,
+	ps.fgs_made,
+	ps.fgs_attempted,
+	ps.three_pts_made,
+	ps.three_pts_attempted,
+	ps.fts_made,
+	ps.fts_attempted,
+	ps.usage_pct
 FROM
 	player_stats AS ps
 JOIN players AS p
@@ -35,7 +44,14 @@ JOIN games AS g
 	ps.game_id = g.game_id
 JOIN teams AS t 
 	ON
-	ps.team_id = t.team_id;
+	ps.team_id = t.team_id
+JOIN teams AS home
+	ON
+	g.home_team_id = home.team_id
+JOIN teams AS away
+	ON
+	g.away_team_id = away.team_id;
+
 -- Create a view for team game total stats
 CREATE VIEW team_game_total AS
 SELECT 
@@ -43,6 +59,12 @@ SELECT
 	ts.team_id,
 	g.game_id,
 	g.game_type,
+	-- Adding opponent team names
+	  CASE
+		WHEN ts.team_id = g.home_team_id
+        THEN away.team_name
+		ELSE home.team_name
+	END AS opponent,
 	ts.total_points,
 	ts.fg_pct,
 	ts.rebounds,
